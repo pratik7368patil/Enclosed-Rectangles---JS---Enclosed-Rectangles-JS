f514b71898af524e733a8c93dc23a05b97865bcb
Pratik Patil <38435193+pratik7368patil@users.noreply.github.com>
2020-11-02 21:04:55 +0530

    change 1

--- src/components/index.js
+++ src/components/index.js
@@ -5,8 +5,20 @@
 //	height: '96px'
 //}
 
-function updateStructure(rec1,rec2){
-	//write your code
+function updateStructure(rec1, rec2) {
+  const newRec = { ...rec1 };
+  newRec.children = [
+    {
+      top: rec2.top - rec1.top,
+      left: rec2.left - rec1.left,
+      height: rec2.height,
+      width: rec2.width,
+      position: "absolute",
+      children: []
+    }
+  ];
+
+  return newRec;
 }
 
 module.exports = updateStructure;
